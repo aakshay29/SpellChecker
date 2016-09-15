30dc9e6b08ffc95f2d1fbd4de8683af8ea9ab248
aakshay29 <aakshay.29@gmail.com>
2016-09-15 11:54:12 -0400

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 //import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		//HashMap hm = new HashMap();
+		ArrayList<String> misspelled = new ArrayList<String>();
 		Scanner sc1 = new Scanner(System.in);
 		System.out.println("Enter a sentence");
 		String input = sc1.nextLine();
@@ -15,13 +16,16 @@ public class Main {
 		for(int i = 0; i < parts.length ; i++){
 			try {
 				boolean checker = sc.Checkker(parts[i].toString());
-				if(checker == false)
+				if(checker == false){
 					System.out.println(parts[i] + " is not a word. Please reenter.");
+					misspelled.add(parts[i]);
+				}
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
 		}
 	
 		sc1.close();
+		System.out.println("\nList of misspelled words: " + misspelled);
 	}
 }
